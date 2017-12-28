13e65465c2d95d6e9fdbcd233647c9da01fed574
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2017-12-28 20:40:57 +0100

    Correct js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 // scripts.js
+window.onerror = function () {
+    alert('Pojawił się błąd. Zajrzyj do logów!');
+};
+
 
 var url = 'http://api.icndb.com/jokes/random';
 
@@ -11,12 +15,37 @@ var paragraph = document.getElementById('joke');
 
 function getJoke() {
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
+
+  xhr.open('GET', url, false);  //czemu tutaj zadziałało dopiero po wpisaniu false i dodaniu https do adresu???
+
+/* ten kod do końca nie działa - status zmienia się i nie odpowiada końcowemu
+  console.log(xhr.status);
+  if(xhr.status == 200 && xhr.status < 300) {
+    console.log("ok");
+  } else {
+    console.log("Błąd podczas ładowania strony\n");
+  }
+*/
   xhr.addEventListener('load', function(){
     var response = JSON.parse(xhr.response);
+
+//    console.log(xhr.response);
+
     paragraph.innerHTML = response.value.joke;
+
+/* pytanie dlaczego nie mogę oprzeć się na responsie?
+    if(xhr.response == null) {
+      console.log("BŁAD");
+    } else {
+      console.log("SUKCES");
+    }
+*/
   });
+
   xhr.send();
+
+//  console.log(xhr.status);
+
 }
 
-$(getJoke)();
+getJoke();
