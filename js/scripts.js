4faca7e1eee0246bb2a7d342c017127cc981a9bf
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2017-12-31 21:35:19 +0100

    Correct js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 // scripts.js
+/*
 window.onerror = function () {
     alert('Pojawił się błąd. Zajrzyj do logów!');
 };
-
+*/
 
 var url = 'https://api.icndb.com/jokes/random';
 
@@ -16,38 +17,19 @@ var paragraph = document.getElementById('joke');
 function getJoke() {
   var xhr = new XMLHttpRequest();
 
-  xhr.open('GET', url, false);  //czemu tutaj zadziałało dopiero po wpisaniu false i dodaniu https do adresu???
+  xhr.open('GET', url);
+  xhr.addEventListener("error", function() {
+    alert("BŁĄD!!!!");
+  });
 
-/* ale kod do końca nie działa - status zmienia się i nie odpowiada końcowemu
-  console.log(xhr.status);
-  if(xhr.status == 200 && xhr.status < 300) {
-    console.log("ok");
-  } else {
-    console.log("Błąd podczas ładowania strony\n");
-  }
-*/
   xhr.addEventListener('load', function(){
     var response = JSON.parse(xhr.response);
 
-    console.log(xhr.response);
-    console.log(xhr.status);
-
     paragraph.innerHTML = response.value.joke;
 
-/* pytanie dlaczego nie mogę oprzeć się na responsie? czy po zmianie adresu w ogóle tutaj js nie dochodzi?
-
-    if(xhr.response == null) {
-      console.log("BŁAD");
-    } else {
-      console.log("SUKCES");
-    }
-
-*/
   });
 
   xhr.send();
-
-  console.log(xhr.status);
 
 }
 
